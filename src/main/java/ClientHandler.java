d0b0b88f35b2a2241449f3910b91c60ddd7a10cd
Jayesh Chaudhari <86295229+jayeshC01@users.noreply.github.com>
2025-09-12 11:03:38 +0530

    Handle API request version v4

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ClientHandler {
-  private Socket clientSocket;
+  private final Socket clientSocket;
+
   ClientHandler(Socket clientSocket) {
     this.clientSocket = clientSocket;
   }
@@ -14,17 +15,43 @@ public class ClientHandler {
       DataInputStream input = new DataInputStream(clientSocket.getInputStream());
       DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
 
-      int message_size = input.readInt(); // 4 bytes
-      input.readShort();       // 2 bytes - requestApiKey
-      short requestApiVersion = input.readShort();   // 2 bytes - requestApiVersion
-      int correlationId = input.readInt();  // 4 bytes
+      int messageSize = input.readInt(); //message size
+      int apiKey = input.readShort(); // apiKey
+      int apiVersion = input.readShort();
+      int correlationId = input.readInt();
 
-      output.writeInt(6);
-      output.writeInt(correlationId);
-      output.writeShort(35); //error code
-      output.flush();
+      short errorCode = 35;
+      if (apiVersion >= 0 && apiVersion <= 4) {
+        errorCode = 0;
+      }
+
+      System.out.println("messageSize: " + messageSize);
+      System.out.println("apiKey: " + apiKey);
+      System.out.println("apiVersion: " + apiVersion);
+      System.out.println("correlationId: " + correlationId);
+      System.out.println("errorCode: " + errorCode);
+
+      sendV4RequestResponse(output, correlationId, errorCode);
     } catch (IOException e) {
+      System.out.printf(e.getMessage());
       throw new RuntimeException(e);
     }
+  }
+
+  private void sendV4RequestResponse(DataOutputStream output, int correlationId, short errorCode) throws IOException {
+    output.writeInt(19); // message size
+    output.writeInt(correlationId); // correlationId
+    // Response body v4
+    output.writeShort(errorCode); // errorCode
+    output.write(2);  // Array Length
+    // Element 1
+    output.writeShort(18); // API key
+    output.writeShort(0); // min version
+    output.writeShort(4); // max version
+    output.write(0); // tag buffer
+    //Element ends
+    output.writeInt(0); // throttle time
+    output.write(0); // tag buffer
+    output.flush();
   }
 }
